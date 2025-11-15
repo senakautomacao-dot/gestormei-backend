0906f6d2302806f272efad6efd14c61f71a355d7
senakautomacao-dot <senakautomacao@gmail.com>
2025-11-15 00:01:54 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,8 @@ router.register(r'compras', CompraViewSet, basename='compra')
 
 urlpatterns = router.urls + [
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('', UserDetailView.as_view(), name='api_root'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('user/', UserDetailView.as_view(), name='user_detail'),
 ]
