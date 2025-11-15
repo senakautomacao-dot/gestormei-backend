afe1493a5acbb35423abcfd54931111695910d12
senakautomacao-dot <senakautomacao@gmail.com>
2025-11-15 00:59:43 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,9 @@
 from rest_framework import routers
-from .views import ReceitaViewSet, CompraViewSet, ProfileView
 from django.urls import path
+from .views import ReceitaViewSet, CompraViewSet
 
 router = routers.DefaultRouter()
 router.register(r'receitas', ReceitaViewSet, basename='receita')
 router.register(r'compras', CompraViewSet, basename='compra')
 
-urlpatterns = router.urls + [
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('', UserDetailView.as_view(), name='api_root'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('user/', UserDetailView.as_view(), name='user_detail'),
-]
+urlpatterns = router.urls
